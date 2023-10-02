04c1b8a727d592027717123b68939bb50215a41b
rodrigovelez99 <rodrigovelez83@gmail.com>
2023-10-01 21:40:57 -0500

    commit1
    commit1

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -51,7 +51,10 @@ fun MarsPhotosApp() {
         ) {
             val marsViewModel: MarsViewModel =
                 viewModel(factory = MarsViewModel.Factory)
-            HomeScreen(marsUiState = marsViewModel.marsUiState)
+            HomeScreen(
+                marsUiState = marsViewModel.marsUiState,
+                retryAction = marsViewModel::getMarsPhotos
+            )
         }
     }
 }
